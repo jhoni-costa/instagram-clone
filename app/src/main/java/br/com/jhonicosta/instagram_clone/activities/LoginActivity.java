cd70f0a13221da4968d41991dac3467544d3e7c2
jhoni-costa <jhoni_costa@outlook.com>
2018-11-05 18:23:14 -0200

    Interface de Cadastro

--- app/src/main/java/br/com/jhonicosta/instagram_clone/activities/LoginActivity.java
+++ app/src/main/java/br/com/jhonicosta/instagram_clone/activities/LoginActivity.java
@@ -1,15 +1,28 @@
 package br.com.jhonicosta.instagram_clone.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import br.com.jhonicosta.instagram_clone.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView cadastrar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        cadastrar = findViewById(R.id.login_txt_cadastrar);
+        cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, CadastroActivity.class));
+            }
+        });
     }
 }
