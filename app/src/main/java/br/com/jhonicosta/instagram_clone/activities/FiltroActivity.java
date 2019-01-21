4fc69b5230ca1ae0f5b61d9ecf19a236b55dbd90
jhoni-costa <jhoni_costa@outlook.com>
2019-01-21 15:37:09 -0200

    Criando interface de filtros

--- app/src/main/java/br/com/jhonicosta/instagram_clone/activities/FiltroActivity.java
+++ app/src/main/java/br/com/jhonicosta/instagram_clone/activities/FiltroActivity.java
@@ -4,6 +4,10 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 import br.com.jhonicosta.instagram_clone.R;
@@ -20,11 +24,42 @@ public class FiltroActivity extends AppCompatActivity {
 
         foto = findViewById(R.id.imagemFoto);
 
+        Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
+        toolbar.setTitle("Filtros");
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close_black_24dp);
+
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
             byte[] dados = bundle.getByteArray("fotoEscolhida");
             imagem = BitmapFactory.decodeByteArray(dados, 0, dados.length);
             foto.setImageBitmap(imagem);
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.ic_salvar_postagem:
+
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_filtro, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return false;
     }
 }
